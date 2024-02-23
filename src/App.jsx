df6b6aefb668966ecf86e8d08afa21c80cecf724
utsav kumar <utsavkumar5462@gmail.com>
2024-02-23 20:33:33 +0530

    major update

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,118 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import UserData from "./UserData";
+import { data } from "autoprefixer";
+// import dotenv from "dotenv";
+// dotenv.config();
 
-const clientId = import.meta.env.VITE_CLIENT_ID;
-const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
-const redirectUri = import.meta.env.VITE_REDIRECT_URI;
-const scope = import.meta.env.VITE_SCOPE;
+// const clientId = process.env.CLIENT_ID;
+// const clientSecret = process.env.CLIENT_SECRET;
+// const redirectUri = process.env.REDIRECT_URI;
+// const scope = process.env.SCOPE;
+const clientId = "121762";
+const clientSecret = "9cc85ab3c82a68efe379aa7c996ecb4ff3c3c190";
+const redirectUri = "http://localhost:5173";
+const scope = "read_all";
 
 const App = () => {
-  // state varibles
   const [accessToken, setAccessToken] = useState("");
-  // const [authorization_code, setauthorization_code] = useState("");
+  const [authHandled, setAuthHandled] = useState(false);
+  const [userData, setUserData] = useState(null);
+  const [atheleteStats, setAtheleteStats] = useState(null);
 
-  // functions
+  const fetchAthleteStats = async (id, token) => {
+    try {
+      const Response = await axios.get(
+        `https://www.strava.com/api/v3/athletes/${id}/stats`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
 
-  const exchageAuthorizationForToken = async (authorizationCode) => {
-    console.log("exchageAuthorizationForToken✅");
-    // console.log(authorizationCode);
-    const tokenResponse = await axios.post(
-      "https://www.strava.com/oauth/token",
-      {
-        client_id: clientId,
-        client_secret: clientSecret,
-        code: authorizationCode,
-        grant_type: "authorization_code",
-      }
-    );
-    console.log("tokenResponse", tokenResponse);
-    // extract the accessToken
-    // const accessToken = tokenResponse
+      console.log("Athlete Stats:", Response.data);
+      setAtheleteStats(Response.data);
+    } catch (error) {
+      console.error("Error fetching athlete stats:", error);
+    }
+  };
+  const fetchAthleteData = async (token) => {
+    try {
+      const Response = await axios.get(
+        "https://www.strava.com/api/v3/athlete",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      console.log("Athlete Data:", Response.data);
+      setUserData(Response.data);
+      console.log(Response.data.id);
+      fetchAthleteStats(Response.data.id, token);
+    } catch (error) {
+      console.error("Error fetching athlete data:", error);
+    }
+  };
+
+  const refreshAccessToken = async (refreshToken) => {
+    try {
+      const tokenResponse = await axios.post(
+        "https://www.strava.com/oauth/token",
+        {
+          client_id: clientId,
+          client_secret: clientSecret,
+          refresh_token: refreshToken,
+          grant_type: "refresh_token",
+        }
+      );
+
+      console.log("Refresh Token Response:", tokenResponse.data);
+      setAccessToken(tokenResponse.data.access_token);
+      fetchAthleteData(tokenResponse.data.access_token);
+    } catch (error) {
+      console.error("Refresh Token Error:", error);
+    }
+  };
+
+  const exchangeAuthorizationForToken = async (authorizationCode) => {
+    try {
+      const tokenResponse = await axios.post(
+        "https://www.strava.com/oauth/token",
+        {
+          client_id: clientId,
+          client_secret: clientSecret,
+          code: authorizationCode,
+        }
+      );
+
+      console.log("Token Exchange Response:", tokenResponse.data);
+      setAccessToken(tokenResponse.data.access_token);
+
+      // Fetch athlete data after obtaining the access token
+      fetchAthleteData(tokenResponse.data.access_token);
+    } catch (error) {
+      console.error("Authorization Error:", error);
+      console.log("Detailed Error:", error.response);
+      // Handle errors as needed
+    }
   };
 
   const handleAuthenticationCallback = () => {
-    console.log("handleAuthenticationCallback✅");
     try {
-      // Parse the URL to extract the authorization code
-      const urlParams = new URLSearchParams(window.location.search);
-      const authorizationCode = urlParams.get("code");
-      console.log("authorizationCode", authorizationCode);
-      if (authorizationCode) {
-        console.log("authorizationCode✅");
-        exchageAuthorizationForToken(authorizationCode);
-      } else {
-        console.error("Authorization code not found in URL");
+      if (!authHandled) {
+        const urlParams = new URLSearchParams(window.location.search);
+        const authorizationCode = urlParams.get("code");
+
+        if (authorizationCode) {
+          exchangeAuthorizationForToken(authorizationCode);
+        } else {
+          console.error("Authorization code not found in URL");
+        }
+
+        setAuthHandled(true);
       }
     } catch (err) {
       console.error("Error in handling authorization callback:", err);
@@ -52,34 +122,54 @@ const App = () => {
   useEffect(() => {
     handleAuthenticationCallback();
   }, []);
+
   const handleConnect = () => {
     const authorizationUrl = `https://www.strava.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}`;
     window.location.href = authorizationUrl;
   };
 
   return (
-    <div className="bg-slate-600 w-full h-screen flex flex-col items-center justify-center">
-      <h1
-        id="headerText"
-        className="w-3/5 text-2xl font-semibold  text-slate-50 tracking-wider"
-      >
-        StravaConnect
-      </h1>
+    <div>
+      {accessToken ? (
+        <>
+          {userData ? (
+            <>
+              <UserData userData={userData} atheleteStats={atheleteStats} />
+            </>
+          ) : (
+            <>
+              <p>Loading user data...</p>
+            </>
+          )}
+        </>
+      ) : (
+        <>
+          <div className="bg-slate-600 w-full h-screen flex flex-col items-center justify-center">
+            <h1
+              id="headerText"
+              className="w-3/5 text-2xl font-semibold  text-slate-50 tracking-wider"
+            >
+              StravaConnect
+            </h1>
 
-      <div className="bg-slate-200 w-3/5 h-3/5 p-12 flex flex-col items-center justify-between rounded-lg ">
-        {/* <UserData /> */}
+            <div className="bg-slate-200 w-3/5 h-3/5 p-12 flex flex-col items-center justify-between rounded-lg ">
+              {/* <UserData /> */}
 
-        <h1 className="w-full bg-slate-900 text-center text-2xl text-red-500 p-4 rounded-lg">
-          Connect to Strava
-        </h1>
-        <button
-          className="w-[90px] h-[30px] bg-green-600 text-xl text-white font-medium font-sans rounded-lg hover:brightness-110 active:translate-y-1 active:shadow-sm"
-          onClick={handleConnect}
-        >
-          Connect
-        </button>
-      </div>
+              <h1 className="w-full bg-slate-900 text-center text-2xl text-red-500 p-4 rounded-lg">
+                Connect to Strava
+              </h1>
+              <button
+                className="w-[90px] h-[30px] bg-green-600 text-xl text-white font-medium font-sans rounded-lg hover:brightness-110 active:translate-y-1 active:shadow-sm"
+                onClick={handleConnect}
+              >
+                Connect
+              </button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
+
 export default App;
--- src/UserData.jsx
+++ src/UserData.jsx
@@ -1,8 +1,86 @@
-const UserData = () => {
+const UserData = (props) => {
+  console.log(props);
+  const {
+    firstname,
+    lastname,
+    bio,
+    sex,
+    city,
+    countryweight,
+    username,
+    profile,
+    resource_state,
+  } = props.userData;
+
+  // const { atheleteStats } = props.atheleteStats;
+
   return (
-    <div className="w-full ">
-      <p>Name: ${}</p>
-      <p>Name: ${}</p>
+    <div className="w-full flex flex-col gap-5">
+      {/* Athelete Details */}
+      <div className="bg-gray-100 w-4/5 h-3/5 rounded p-5 ">
+        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold ml-4">
+          Athelete Details
+        </div>
+        <div className=" rounded-lg p-4 shadow-md flex items-center space-x-56">
+          <img
+            src={profile}
+            alt={"profile_img"}
+            className="w-36 h-36 rounded-full object-cover"
+          />
+          <div>
+            <h2 className="text-xl font-semibold">
+              {firstname} {lastname}
+            </h2>
+            <p className="mt-2 text-gray-600">Sex: {sex}</p>
+            <p className="mt-2 text-gray-600">Stat: 30</p>
+            <p className="mt-2 text-gray-600">City: {city}</p>
+          </div>
+        </div>
+      </div>
+      {/* Athelete Stats */}
+      <div>
+        <div className="bg-gray-100 w-4/5 h-3/5 rounded-lg p-5">
+          <div className="uppercase tracking-wide text-sm text-orange-500 font-semibold ml-4">
+            Athelete Stats
+          </div>
+          {/* <div>
+            <h3 className="text-md font-semibold mb-2">All Ride Totals</h3> */}
+          {/* <ul className="list-disc pl-6">
+              {Object.entries(athleteStats.all_ride_totals).map(
+                ([key, value]) => (
+                  <li key={key}>
+                    {key}: {value}
+                  </li>
+                )
+              )}
+            </ul>
+          </div> */}
+          {/* <div>
+            <h3 className="text-md font-semibold mb-2">All Run Totals</h3>
+            <ul className="list-disc pl-6">
+              {Object.entries(athleteStats.all_run_totals).map(
+                ([key, value]) => (
+                  <li key={key}>
+                    {key}: {value}
+                  </li>
+                )
+              )}
+            </ul>
+          </div>
+          <div>
+            <h3 className="text-md font-semibold mb-2">All Swim Totals</h3>
+            <ul className="list-disc pl-6">
+              {Object.entries(athleteStats.all_swim_totals).map(
+                ([key, value]) => (
+                  <li key={key}>
+                    {key}: {value}
+                  </li>
+                )
+              )}
+            </ul>
+          </div> */}
+        </div>
+      </div>
     </div>
   );
 };
