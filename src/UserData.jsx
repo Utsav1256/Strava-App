8fff75c8b0fe21a92659a99acc1364139d5b6409
utsav kumar <utsavkumar5462@gmail.com>
2024-02-25 18:20:26 +0530

    add logout

--- src/UserData.jsx
+++ src/UserData.jsx
@@ -19,22 +19,23 @@ const UserData = (props) => {
 
   console.log("athleteStats", athleteStats);
 
+  const handleLogout = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
+
   return (
     <div className="w-full flex flex-col gap-5 items-center justify-center">
       {/* Athelete Details */}
-      <div className="bg-gray-100 w-4/5 h-3/5 rounded p-5 mt-5">
-        <div
-          className="uppercase trackin
-        
-        g-wide text-sm text-indigo-500 font-semibold ml-4"
-        >
+      <div className="bg-gray-100 w-full md:w-4/5 h-auto md:h-3/5 rounded p-5 mt-5">
+        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold ml-4">
           Athelete Details
         </div>
-        <div className=" rounded-lg p-4 shadow-md flex items-center space-x-56">
+        <div className="rounded-lg p-4 shadow-md flex flex-col md:flex-row items-center md:space-x-6">
           <img
             src={profile}
             alt={"profile_img"}
-            className="w-36 h-36 rounded-full object-cover"
+            className="w-36 h-36 rounded-full object-cover mb-4 md:mb-0 md:w-48 md:h-48"
           />
           <div>
             <h2 className="text-xl font-semibold">
@@ -44,7 +45,6 @@ const UserData = (props) => {
               Username: {username ? username : "Not available"}
             </p>
             <p className="mt-2 text-gray-600">Sex: {sex}</p>
-
             <p className="mt-2 text-gray-600">
               Weight: {weight ? weight : "Not Available"}
             </p>
@@ -57,7 +57,7 @@ const UserData = (props) => {
         </div>
       </div>
       {/* Athelete Stats */}
-      <div className="bg-gray-100 w-4/5 h-3/5 rounded-lg p-6">
+      <div className="bg-gray-100 w-full md:w-4/5 h-auto md:h-3/5 rounded p-5 mt-5">
         <div className="uppercase tracking-wide text-sm text-orange-500 font-semibold ml-4 mb-4">
           Athelete Stats
         </div>
@@ -153,6 +153,12 @@ const UserData = (props) => {
                 </div>
               </div>
             </div>
+            <button
+              className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded"
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
           </>
         ) : (
           <>Loading...</>
